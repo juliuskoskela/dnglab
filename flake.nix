09f5bebc57c5768daa8a40f6b4fec3b31b393558
Julius Koskela <me@juliuskoskela.dev>
2023-11-02 19:42:42 +0200

    Implement dnglab flake.nix
    Signed-off-by: Julius Koskela <me@juliuskoskela.dev>

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Dnglab";
+
+  inputs = {
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    flake-utils.follows = "cargo2nix/flake-utils";
+    nixpkgs.follows = "cargo2nix/nixpkgs";
+  };
+
+  outputs = inputs: with inputs;
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [cargo2nix.overlays.default];
+        };
+
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          rustVersion = "1.73.0";
+          packageFun = import ./Cargo.nix;
+        };
+
+      in rec {
+        packages = {
+          dnglab = (rustPkgs.workspace.dnglab {});
+          default = packages.dnglab;
+        };
+      }
+    );
+}
